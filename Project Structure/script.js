073080bd708e33e4e708eb2bf3d810b6d376a42c
AriaRazavi2005 <ariarazavi105@gmail.com>
2025-10-30 19:52:04 +0330

    exHanoi_2 done

--- Project Structure/script.js
+++ Project Structure/script.js
@@ -60,9 +60,12 @@ function exHanoi_1(start, aux, end, n) {
 }
 
 function exHanoi_2(A, B, C, D, n) {
-    alert("your function is not complete")
-    return
+ 
+    hanoi(A, D, B, n);
 
+    hanoi(C, D, A, n);
+
+    hanoi(B, D, C, n);
 }
 
 function exhanoi_3(A, B, C, n) {
